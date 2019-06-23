9efbb059076a1369d796d44401eb8abc024dc778
Aleksandr <adorofeev@team.amocrm.com>
2019-06-23 23:49:49 +0300

    start maket

---
+++ README.md
@@ -0,0 +1,4 @@
+SpaceX Graphql app.
+
+[X] Create new workflow
+[X] Add Apollo
--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/"
+})
+
+client.query({
+  query:gql`
+  {
+    launchesPast(limit: 10) {
+      mission_name
+      launch_date_local
+    }
+  }
+  `
+})
+.then(result => console.log(result))
 
 const App = () => (
   <div>Hello!</div>
